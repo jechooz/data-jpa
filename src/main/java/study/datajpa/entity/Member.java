df18fb7c08f8d9595a571276b0ffde52ddb504b8
jechooz <jieunchc@gmail.com>
2024-07-19 11:38:52 +0900

    first commit

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,36 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.AccessLevel;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+
+}
